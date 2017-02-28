00db2c29151fee53d5e5a61d3c144f1722e42c2a
jouncealimb <danicasapit@gmail.com>
2017-02-28 01:11:32 -0500

    archery site first push

---
+++ js/navbar.js
@@ -0,0 +1,94 @@
+document.getElementById("navMenu").innerHTML =
+'	<div style="text-align: right">' +
+	'<text style="font-size: 12px">7 Sherman Ave, Floor 2, Jersey City, NJ 07307 &bull; (201) 420-9700 &bull;</style>' +
+	'</div>' +
+	
+
+'	<section class="header bg-black header-1" >' +
+'			<div class="topbar " data-effect="fadeIn" id="top">' +
+'				<div class="menu"> ' +
+'					<div class="primary inviewport animated delay4" data-effect="fadeInRightBig"> ' +
+'						<div class='cssmenu'> ' +
+								
+'		<ul class='menu-ul'><li class='has-sub'>' + 
+'										<a href='#'>Home </a>
+</li>
+<li class='has-sub'>
+										<a href='#'>About <span class="caret"></span></a>
+<ul>
+<li>
+											<a href='aboutJCA.html'>About JCA</a>
+											</li>
+<li>
+											<a href='team.html'>Meet the Team</a>
+											</li>
+</ul>
+</li>
+<li class='has-sub'>
+										<a href='#'>Classes <span class="caret"></span></a>
+<ul>
+<li>
+											<a href='services.html'>Services</a>
+											</li>
+<li>
+											<a href='reserve.html'>Reserve a class</a>
+											</li>
+<li>
+											<a href='#'>Beginner Classes</a>
+											</li>
+
+</ul>
+</li>
+</ul>
+		<!-- Menu - End -->
+								</div>
+					</div>
+					<div class="black inviewport animated delay4" data-effect="fadeInLeftBig">
+						<div class='cssmenu'>
+								
+		<!-- Menu - Start -->
+		<ul class='menu-ul'><li class='has-sub'>
+										<a href='#'>Activities <span class="caret"></span></a>
+											<ul>
+											<li>
+											<a href='calendar.html'>Events Calendar</a>
+											</li>
+											<li>
+											<a href='leagues.html'>Leagues</a>
+											</li>											
+											<li>
+											<a href='party.html'>Party Events</a>
+											</li>
+											<li>
+											<a href='corporate.html'>Corporate Events</a>
+											</li>
+</ul>
+</li>
+<li class='has-sub'>
+										<a href='contact.html'>Contact Us</a>
+									</li>
+</ul>
+		<!-- Menu - End -->
+								</div>						
+					</div>
+				</div>
+			</div>
+		<!-- Menu Top Bar - End -->
+
+		<!-- Logo and Mobile Menu - Start -->
+			<div class='header-logo-wrap'>
+				<div class="container">
+					<div class="col-xs-2">
+						<span><img src="img/logo.png" style="width: 300px; height: 100px"></span>
+						<!--<span>Jersey City Archery</span>-->
+					</div>
+					<div class="menu-mobile col-xs-10 pull-right cssmenu">
+						<i class="mdi mdi-menu menu-toggle"></i>
+						<ul class="menu" id='parallax-mobile-menu'>
+						
+						</ul>
+					</div>
+
+				</div>
+			</div>
+		<!-- Logo and Mobile Menu - End -->'
